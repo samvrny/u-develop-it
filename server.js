7a81f729206eb7702c12e5e77305ce6d7fd61203
samvrny <samvrny@gmail.com>
2022-08-09 16:45:09 -0500

    made query statements

--- server.js
+++ server.js
@@ -20,9 +20,35 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+//create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//              VALUES (?,?,?,?)`;
+// const params = [1, 'Sharon', 'Marsh', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 //default response for any other request (not found) THIS HAS TO BE AFTER ALL OTHER REQUESTS otherwise it will override them all.
 app.use((req, res) => {
