448de1b71a3250ca406c02b9d67e89ce38e0d06c
samvrny <samvrny@gmail.com>
2022-08-09 13:53:20 -0500

    made a query to the SQL database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,22 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        //Your MySQL passwork
+        password: '1@3fp7!ARh',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (not found) THIS HAS TO BE AFTER ALL OTHER REQUESTS otherwise it will override them all.
 app.use((req, res) => {
