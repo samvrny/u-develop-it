f74ab56f60b0c542f7be2c9ed9d9ee2e8e77f0b2
samvrny <samvrny@gmail.com>
2022-08-10 15:22:03 -0500

    JOINED two tables, parties and candidates

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,22 @@
-INSERT INTO candidates (first_name, last_name, industry_connected)
-VALUES
-    ('Sharon', 'Marsh', 1),
-    ('Alex', 'Mason', 1),
-    ('Randy', 'Randerson', 0),
-    ('Deandra', 'Reynolds', 0),
-    ('Loba', 'Andrade', 1),
-    ('Arthur', 'Morgan', 0),
-    ('John', 'Price', 1),
-    ('Lois', 'Griffin', 1),
-    ('Cyril', 'Figgus', 0),
-    ('Leslie', 'Knope', 1),
-    ('Nandor', 'Relentless', 0),
-    ('Sadie', 'Adler', 1),
-    ('Marge', 'Simpson', 1);
-----------------------------------------------------------------------
 INSERT INTO parties (name, description)
 VALUES
     ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
     ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
     ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
+
+INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
+VALUES
+    ('Sharon', 'Marsh', 1, 1),
+    ('Alex', 'Mason', 1, 1),
+    ('Randy', 'Randerson', 1, 0),
+    ('Jim', 'Milton', 1, 1),
+    ('Deandra', 'Reynolds', 2, 0),
+    ('Loba', 'Andrade', 2, 1),
+    ('Arthur', 'Morgan', 2, 0),
+    ('John', 'Price', 2, 1),
+    ('Lois', 'Griffin', NULL, 1),
+    ('Cyril', 'Figgus', 3, 0),
+    ('Leslie', 'Knope', 3, 1),
+    ('Nandor', 'Relentless', 3, 0),
+    ('Sadie', 'Adler', 3, 1),
+    ('Marge', 'Simpson', 3, 1);
